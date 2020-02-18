357adbacb57fe8cbccdf3ad2c80328f6bf8dfe28
DESKTOP-AVTI26A\burto <matthewburton@webster.edu>
2020-02-17 20:46:19 -0600

    Made teleport work in game. Added sweet camera folowing logic to improve overall look as well as make it less jarring when player teleports

---
+++ FireWall Project/Assets/matt-week-5/TeleportV2.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportV2 : MonoBehaviour
+{
+    private Transform PlayerTransform;
+
+    public float distanceToTeleport;
+
+    void Start()
+    {
+        PlayerTransform = GameObject.Find("Player").transform;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            Vector3 teleportPosition = new Vector3(PlayerTransform.position.x + distanceToTeleport, PlayerTransform.position.y, 0);
+            PlayerTransform.position = teleportPosition;
+        }
+    }
+}
